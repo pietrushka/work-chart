0c6db096b74f71c37b76dbaf11f389feb8c27c0d
pietrushka <piotrekwisnia18@gmail.com>
2025-08-07 23:23:29 +0200

    feat: minor Dashboard improvements

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,14 +1,28 @@
-import { Button } from "@mui/material"
 import { Link } from "react-router"
+import { Button, Box } from "@mui/material"
 import { useLogoutMutation } from "../redux/api/authApi"
 
 export default function Dashboard() {
   const [logout] = useLogoutMutation()
+
   return (
-    <>
-      <Link to="/employees">Employees</Link>
-      <Link to="/shift-template">Shift Template</Link>
-      <Button onClick={() => logout()}>Logout</Button>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "end",
+      }}
+    >
+      <Box sx={{ display: "flex", gap: 1 }}>
+        <Button component={Link} to="/employees" variant="contained">
+          Employees
+        </Button>
+        <Button component={Link} to="/shift-template" variant="contained">
+          Shift Template
+        </Button>
+        <Button onClick={() => logout()} variant="outlined" color="error">
+          Logout
+        </Button>
+      </Box>
+    </Box>
   )
 }
